d7368b8380f258645281310f81d321e3a2cfd64d
manish <manish25.mv@gmail.com>
2024-09-03 20:22:52 +0530

    contact-list

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Header.module.css"
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+        <p> Contacts-List Form </p>
+    </div>
+  )
+}
+export default Header;
